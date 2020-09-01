ffa45137db588513fff53bbb97da33b7f5b8478f
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-01 08:14:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,7 +47,7 @@ def monopoly_with_fourth_tier
     }
    }
 }
-:railroads[:names][:reading_railroad][:mortgage_value => 100]
+:railroads[:names][:reading_railroad][:mortgage_value] = 100
 mon
 end
 
